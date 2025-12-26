b637aeaddabbac9fe67e061e2e141a8789d94175
Aamar <m.aamar@compassint.org>
2025-12-26 17:53:00 +0400

    fix git ignore

--- src/CleanArchitecture.OCR.API/obj/Debug/net8.0/CleanArchitecture.OCR.API.AssemblyInfo.cs
+++ src/CleanArchitecture.OCR.API/obj/Debug/net8.0/CleanArchitecture.OCR.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.OCR.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c1fe25a1c0839db6811aca16bbc4c53b6ba6752")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.OCR.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.OCR.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
